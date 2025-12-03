acfaa0fce9d0779de1b27d8e405e284fdb0fe3f2
DESKTOP-N7SRS7G\HARI <harikrishnanr.dev@gmail.com>
2025-12-04 01:37:04 +0530

    fix: update image configuration to include remote patterns for vkvimplantclinic.com

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    images: {
-    domains: ['localhost'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "vkvimplantclinic.com",
+        port: "",
+        pathname: "/backend/api/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "vkvimplantclinic.com",
+        port: "",
+        pathname: "/backend/api/uploads/**",
+      },
+    ],
+    domains: ["localhost"], // keep your localhost support
   },
 };
 
